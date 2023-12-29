425e60aa3e1301c9ada056201580bc8c1ef01564
nathanael <lecron.nathanael@gmail.com>
2023-12-29 20:37:54 +0100

    todo : projects carousel and performance and menu links

--- src/App.js
+++ src/App.js
@@ -4,9 +4,21 @@ import Preloader from "./components/preLoader";
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import FullScreenMenu from "./components/FullScreenMenu";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useGLTF, Stage, PresentationControls } from "@react-three/drei";
+
+function Model(props) {
+  const { scene } = useGLTF("./keyboard.glb");
+  // Use useFrame to rotate the model continuously
+  useFrame(() => {
+    // Rotate the model on the Y-axis (you can adjust the rotation speed as needed)
+    scene.rotation.y += 0.01;
+    scene.rotation.x += 0.01;
+  });
+  return <primitive object={scene} {...props} />;
+}
 
 function App() {
-
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleMenu = () => {
@@ -14,51 +26,50 @@ function App() {
   };
 
   const [mousePosition, setMousePosition] = useState({
-    x:0,
-    y:0
+    x: 0,
+    y: 0,
   });
 
-  
   const [cursorVariant, setCursorVariant] = useState("default");
 
   useEffect(() => {
-    const mouseMove = e => {
+    const mouseMove = (e) => {
       setMousePosition({
         x: e.clientX,
-        y: e.clientY
-      })
-    }
+        y: e.clientY,
+      });
+    };
 
     window.addEventListener("mousemove", mouseMove);
 
     return () => {
       window.removeEventListener("mousemove", mouseMove);
-    }
+    };
   }, []);
 
   const variants = {
-    default : {
+    default: {
       x: mousePosition.x - 16,
       y: mousePosition.y - 16,
-      backgroundColor: isMenuOpen ? "black" : "white"
+      backgroundColor: "white",
     },
-    text : {
-      height:150,
-      width:150,
+    text: {
+      height: 150,
+      width: 150,
       x: mousePosition.x - 75,
       y: mousePosition.y - 75,
       backgroundColor: "white",
-      mixBlendMode: "difference"
+      mixBlendMode: "difference",
     },
-    menu : {
-      height:150,
-      width:150,
+    menu: {
+      height: 150,
+      width: 150,
       x: mousePosition.x - 75,
       y: mousePosition.y - 75,
       backgroundColor: "white",
-      mixBlendMode: "difference"
-    }
-  }
+      mixBlendMode: "difference",
+    },
+  };
 
   const textEnter = () => setCursorVariant("text");
   const textLeave = () => setCursorVariant("default");
@@ -66,39 +77,119 @@ function App() {
   const menuEnter = () => setCursorVariant("menu");
   const menuLeave = () => setCursorVariant("default");
 
-  
-
   return (
     <>
       <Preloader />
       <div className="hline"></div>
-      <motion.div className="cursor" variants={variants} animate={cursorVariant}></motion.div>
-      <div className="Menu" onMouseEnter={menuEnter} onMouseLeave={menuLeave} onClick={toggleMenu}>.menu</div>
+      <motion.div
+        className="cursor"
+        variants={variants}
+        animate={cursorVariant}
+      ></motion.div>
+      <div
+        className="Menu"
+        onMouseEnter={menuEnter}
+        onMouseLeave={menuLeave}
+        onClick={toggleMenu}
+      >
+        .menu
+      </div>
       <FullScreenMenu isOpen={isMenuOpen} onClose={toggleMenu} />
-      <div onMouseEnter={textEnter} onMouseLeave={textLeave} className="name">Nathanaël <br /> Lecron</div>
-      <img src="https://media.vogue.fr/photos/5d31bf328133af0008628de6/1:1/w_4617,h_4617,c_limit/GettyImages-486778371.jpg"
-      alt="ds" height="500" width="500" />
+      <div onMouseEnter={textEnter} onMouseLeave={textLeave} className="name">
+        Nathanaël <br /> Lecron
+      </div>
+      <img
+        src="https://media.vogue.fr/photos/5d31bf328133af0008628de6/1:1/w_4617,h_4617,c_limit/GettyImages-486778371.jpg"
+        alt="ds"
+        height="500"
+        width="500"
+      />
       <div className="landing">
         <div className="desc">
-          I am a french student who as a fierce and profound curiosity in computer science and who whishes to make software engineer
-          his job. I am currently enrolled in a B.S in computer science at the Orléans University and will graduate in 2026. I am 
-          enthusiastic about exploring new technologies and collaborating with experienced professionals to create meaningful experiences.
+          I am a french student who as a fierce and profound curiosity in
+          computer science and who whishes to make software engineer his job. I
+          am currently enrolled in a B.S in computer science at the Orléans
+          University and will graduate in 2026. I am enthusiastic about
+          exploring new technologies and collaborating with experienced
+          professionals to create meaningful experiences.
         </div>
+
+        <Canvas
+          dpr={[1, 2]}
+          shadows
+          camera={{ fove: 45 }}
+          style={{
+            position: "absolute",
+            width: "800px",
+            height: "450px",
+            top: "760px",
+            right: "100px",
+            zIndex: "-1",
+          }}
+        >
+          <color attach="background" args={["#000000"]} />
+          <PresentationControls speed={1.5} zoom={0.5}>
+            <Stage environment={"city"}>
+              <Model scale={0.015} />
+            </Stage>
+          </PresentationControls>
+        </Canvas>
+
         <div class="marquee">
           <div class="track">
             <div class="content">
-              &nbsp;DEVELOPER PASSIONNATE STUDENT CURIOUS LEARNER DEVELOPER PASSIONNATE STUDENT CURIOUS LEARNER DEVELOPER 
-              PASSIONNATE STUDENT CURIOUS LEARNER
+              &nbsp;DEVELOPER PASSIONNATE STUDENT CURIOUS LEARNER DEVELOPER
+              PASSIONNATE STUDENT CURIOUS LEARNER DEVELOPER PASSIONNATE STUDENT
+              CURIOUS LEARNER
             </div>
           </div>
         </div>
       </div>
       <div className="hline2"></div>
-      <div className="projects">Some of my projects
-        <div className="project1">Project 1</div>
-        <div className="project2">Project 2</div>
-        <div className="project3">Project 3</div>
-      </div>
+      <div className="projects">Some of my projects</div>
+        <div className="project1">
+          Weather Windows App
+          <div className="p1-content">
+            I developed a straightforward weather application using Electron,
+            which allowed me to create a Windows application using HTML and CSS.
+            This project proved to be instrumental in reinforcing my proficiency
+            in HTML and CSS, providing a practical application for the
+            theoretical knowledge I had acquired. Moreover, during the
+            development process, I delved into the utilization of version
+            control tools, particularly Git, which significantly enhanced my
+            understanding of collaborative coding and project management.
+            Through this experience, I not only expanded my technical skills but
+            also gained valuable insights into the importance of efficient
+            version control in software development.
+          </div>
+        </div>
+        <div className="project2">
+          C++ 3D Graphics
+          <div className="p2-content">
+            I engaged in the creation of diverse shaders and explored 3D
+            rendering using the OpenGL library in conjunction with the C++
+            programming language. This endeavor proved to be pivotal in
+            deepening my comprehension of computer graphics, providing hands-on
+            experience in leveraging the powerful capabilities of OpenGL for
+            graphical rendering tasks. Additionally, the project served as a
+            valuable platform for enhancing my proficiency in the C++ language,
+            allowing me to apply its features and principles in a practical
+            context.
+          </div>
+        </div>
+        <div className="project3">
+          Python Suspension simulation
+          <div className="p3-content">
+            I developed a Python simulation for a mechanically accurate vehicle
+            suspension system, mimicking its response across diverse terrains.
+            This project not only strengthened my Python skills but also
+            showcased the practical application of programming in addressing
+            real-world challenges. By connecting theoretical knowledge to
+            tangible problems, I gained valuable insights into both Python's
+            capabilities and the transformative potential of programming in
+            practical scenarios.
+          </div>
+        </div>
       <div className="footer">© 2024 Nathanaël Lecron</div>
     </>
   );
--- src/components/FullScreenMenu.js
+++ src/components/FullScreenMenu.js
@@ -1,12 +1,134 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import "./FullScreenMenu.css";
 
 const FullScreenMenu = ({ isOpen, onClose }) => {
   const menuVariants = {
-    hidden: { opacity: 1, y: "100%" },
-    visible: { opacity: 1, y: 0 },
+    hidden: {
+      opacity: 1,
+      y: "100%",
+      transition: {
+        opacity: { duration: 0.5 },
+        y: { duration: 0.5 },
+        ease: "ease-in",
+      },
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        opacity: { duration: 0.5 },
+        y: { duration: 0.5 },
+        ease: "ease-in",
+      },
+    },
   };
+
+  const textVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: (index) => ({
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.8, delay: index * 0.4 }, // Adjust the delay factor
+    }),
+  };
+
+  const [mousePosition, setMousePosition] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  const [cursorVariant, setCursorVariant] = useState("default");
+
+  useEffect(() => {
+    const mouseMove = (e) => {
+      setMousePosition({
+        x: e.clientX,
+        y: e.clientY,
+      });
+    };
+
+    window.addEventListener("mousemove", mouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+    };
+  }, []);
+
+  const variants = {
+    default: {
+      x: mousePosition.x - 16,
+      y: mousePosition.y - 16,
+      backgroundColor: "black",
+    },
+    text: {
+      height: 150,
+      width: 150,
+      x: mousePosition.x - 75,
+      y: mousePosition.y - 75,
+      backgroundColor: "white",
+      mixBlendMode: "difference",
+    },
+    menu: {
+      height: 150,
+      width: 150,
+      x: mousePosition.x - 75,
+      y: mousePosition.y - 75,
+      backgroundColor: "white",
+      mixBlendMode: "difference",
+    },
+  };
+
+  const preloadedImage1 = new Image();
+  preloadedImage1.src = "https://media.vogue.fr/photos/5d31bf328133af0008628de6/1:1/w_4617,h_4617,c_limit/GettyImages-486778371.jpg";
+  const preloadedImage2 = new Image();
+  preloadedImage2.src = "https://upload.wikimedia.org/wikipedia/commons/5/5d/Pissarro_Camille_-_Boulevard_Montmartre_%C3%A0_Paris.jpg";
+  const preloadedImage3 = new Image();
+  preloadedImage3.src = "https://media.vogue.fr/photos/64c9182ab03f46b17f0da72c/2:3/w_2560%2Cc_limit/12.Strada%2520Romana%2520Bordighera.jpg";
+  
+  
+
+  const hoverMenu1 = () => {
+    const fsmElement = document.getElementById("fsm");
+    fsmElement.style.background =`url(${preloadedImage1.src})`;
+    fsmElement.style.backgroundSize = "cover";
+    fsmElement.style.backgroundPosition = "center";
+    setCursorVariant("menu");
+  };
+
+  const unhoverMenu1 = () => {
+    document.getElementById("fsm").style.background = "";
+    setCursorVariant("default");
+  };
+
+  const hoverMenu2 = () => {
+    const fsmElement = document.getElementById("fsm");
+    fsmElement.style.background = `url(${preloadedImage2.src})`;
+    fsmElement.style.backgroundSize = "cover";
+    fsmElement.style.backgroundPosition = "center";
+    setCursorVariant("menu");
+  };
+
+  const unhoverMenu2 = () => {
+    document.getElementById("fsm").style.background = "";
+    setCursorVariant("default");
+  };
+
+  const hoverMenu3 = () => {
+    const fsmElement = document.getElementById("fsm");
+    fsmElement.style.background =`url(${preloadedImage3.src})`;
+    fsmElement.style.backgroundSize = "cover";
+    fsmElement.style.backgroundPosition = "center";
+    setCursorVariant("menu");
+  };
+
+  const unhoverMenu3 = () => {
+    document.getElementById("fsm").style.background = "";
+    setCursorVariant("default");
+  };
+
+  const menuEnter = () => setCursorVariant("menu");
+  const menuLeave = () => setCursorVariant("default");
 
   return (
     <AnimatePresence>
@@ -17,10 +139,55 @@ const FullScreenMenu = ({ isOpen, onClose }) => {
           initial="hidden"
           animate="visible"
           exit="hidden"
+          id="fsm"
         >
           {/* Your menu content goes here */}
-          <div onClick={onClose} className="close-button">
-            Close Menu
+          <motion.div
+            className="cursor"
+            variants={variants}
+            animate={cursorVariant}
+          ></motion.div>
+          <motion.div variants={textVariants} className="menu-text">
+            <ul>
+              <motion.li variants={textVariants} custom={0}>
+                <a
+                  href=""
+                  className="menu-item"
+                  onMouseEnter={hoverMenu1}
+                  onMouseLeave={unhoverMenu1}
+                >
+                  Home
+                </a>
+              </motion.li>
+              <motion.li variants={textVariants} custom={1}>
+                <a
+                  href=""
+                  className="menu-item"
+                  onMouseEnter={hoverMenu2}
+                  onMouseLeave={unhoverMenu2}
+                >
+                  Projects
+                </a>
+              </motion.li>
+              <motion.li variants={textVariants} custom={2}>
+                <a
+                  href=""
+                  className="menu-item"
+                  onMouseEnter={hoverMenu3}
+                  onMouseLeave={unhoverMenu3}
+                >
+                  Contact
+                </a>
+              </motion.li>
+            </ul>
+          </motion.div>
+          <div
+            onClick={onClose}
+            onMouseEnter={menuEnter}
+            onMouseLeave={menuLeave}
+            className="close-button"
+          >
+            .close
           </div>
         </motion.div>
       )}
