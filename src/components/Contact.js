907fc1fa5288b5385995579ea2da2f28dd372961
Nathanaël Lecron <natoudu91@gmail.com>
2024-01-22 19:53:49 +0100

    added functionnal emailjs contact, need to style it and make it more clear

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,7 @@
 import React, { useRef } from 'react';
+import { useEffect, useState } from 'react';
 import emailjs from '@emailjs/browser';
+import "./Contact.css"
 
 export const Contact = () => {
   const form = useRef();
@@ -7,13 +9,90 @@ export const Contact = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+    emailjs.sendForm('contact_service', 'contact_form', form.current, '3wcOjOzauDMKp9ULa')
       .then((result) => {
           console.log(result.text);
       }, (error) => {
           console.log(error.text);
       });
   };
+  const menuVariants = {
+    hidden: {
+      opacity: 1,
+      y: "100%",
+      transition: {
+        opacity: { duration: 0.5 },
+        y: { duration: 0.5 },
+        ease: "ease-in",
+      },
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        opacity: { duration: 0.5 },
+        y: { duration: 0.5 },
+        ease: "ease-in",
+      },
+    },
+  };
+
+  const textVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: (index) => ({
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.8, delay: index * 0.4 }, // Adjust the delay factor
+    }),
+  };
+
+  const [mousePosition, setMousePosition] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  const [cursorVariant, setCursorVariant] = useState("default");
+
+  useEffect(() => {
+    const mouseMove = (e) => {
+      setMousePosition({
+        x: e.clientX,
+        y: e.clientY,
+      });
+    };
+
+    window.addEventListener("mousemove", mouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+    };
+  }, []);
+
+  const variants = {
+    default: {
+      x: mousePosition.x - 16,
+      y: mousePosition.y - 16,
+      backgroundColor: "black",
+    },
+    text: {
+      height: 150,
+      width: 150,
+      x: mousePosition.x - 75,
+      y: mousePosition.y - 75,
+      backgroundColor: "white",
+      mixBlendMode: "difference",
+    },
+    menu: {
+      height: 150,
+      width: 150,
+      x: mousePosition.x - 75,
+      y: mousePosition.y - 75,
+      backgroundColor: "white",
+      mixBlendMode: "difference",
+    },
+  };
+  const textEnter = () => setCursorVariant("menu");
+  const textLeave = () => setCursorVariant("default");
 
   return (
     <form ref={form} onSubmit={sendEmail}>
@@ -23,7 +102,7 @@ export const Contact = () => {
       <input type="email" name="user_email" />
       <label>Message</label>
       <textarea name="message" />
-      <input type="submit" value="Send" />
+      <input type="submit" value="Send" onMouseEnter={textEnter} onMouseLeave={textLeave} />
     </form>
   );
 };
